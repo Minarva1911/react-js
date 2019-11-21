defbcf4d7d009176f76b288209d3d5f688dce758
minarva <minarvamohan@gmail.com>
2019-11-21 16:54:40 +0530

    state and props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Userinput from './Components/Userinput/Userinput';
+import Useroutput from './Components/Useroutput/Useroutput';
+// import Signup from './Components/Signup/Signup';
+// import Signin from './Components/Signin/Signin';
+// import Lifecycle from './Components/Lifecycle/Lifecycle';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+
+class App extends Component {
+  state={
+    person:[{
+        name:"Minarva",age:"22",
+    }]
 }
+buttonClickHandler=()=>{
+  console.log('clicked');
+this.setState({
+  person:[{name:"Mohan",age:"25"}]
+})
+}
+  render() {
+    return (
+      <div>
+      <Userinput/>
+      <Useroutput name='Qwinix Technology'/>
+      <Useroutput name='Qwinix Technology'/>
+      <button onClick={this.buttonClickHandler}>click here</button>
+      <p>I am {this.state.person[0].name} my age is{this.state.person[0].age}</p>
+      </div>
+    );
+    }
+  }
+
 
 export default App;
